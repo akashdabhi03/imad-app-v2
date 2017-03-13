705792255686e9cd6a01eeba11071f07641d974c
akashdabhi03 <akashdabhi03@yahoo.com>
2017-03-13 16:05:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,22 @@
-console.log('Loaded!');
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    var request = new XMPHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMPHttpRequest.DONE) {
+            if (request.status === 200) {
+                alert('Logged in successfully');
+            } else if (request.status === 403) {
+                alert('Username/password is incorrect');
+            } else if (request.status === 500) {
+                alert('Something went wrong on the server');
+            }
+        }
+    };
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://akashdabhi03.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
